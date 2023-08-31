5de2e28d76de23816f8bb2de51e76f56083cd6c7
Bilal10252 <arshadbilal1025@gmail.com>
2023-08-31 19:44:53 -0400

    Refactoring the code

--- main.js
+++ main.js
@@ -11,33 +11,53 @@ function show(){
 let sections = document.querySelectorAll("section");
 let navLinks = document.querySelectorAll("header nav ul li a");
 
-// Here window.onscoll means window.addEventListener("scroll", function);
-window.onscroll =() => {
-    sections.forEach(function(sec){
-        let top = window.scrollY;
-        let offset = sec.offsetTop -100;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute("id");
 
-        if(top >= offset && top < offset + height){
-            navLinks.forEach(
-                function(links){
-                    links.classList.remove("active");
-                    document.querySelector('header nav ul li a[href*= ' + id + ']').classList.add("active");
-                }
-            )
 
+
+window.addEventListener("scroll", liveScrolling);
+
+
+ function liveScrolling  ()  {
+    let current = "";
+    let scrollY = window.scrollY;
+
+    sections.forEach(section => {
+        let sectionTop = section.offsetTop -100;
+        let sectionHeight = section.offsetHeight;
+
+        console.log("ScrollY: "+ scrollY);
+        console.log("sectionTop: "+ sectionTop);
+        console.log("sectionHeight: "+ sectionHeight);
+
+        if(scrollY >= sectionTop && scrollY <= (sectionTop+sectionHeight)){
+            current = section.getAttribute("id");
         }
+
+
+        navLinks.forEach(link => {
+            link.classList.remove("active");
+            if(link.classList.contains(current)){
+                link.classList.add("active");
+            }
+        })
+        
+
+    })
+
+
+}
+
+
         let header = document.querySelector("header");
         header.classList.toggle("sticky", window.scrollY > 100);
 
         menuicon.classList.remove("bx-x");
         navbar.classList.remove("active");
 
-    })
+    // })
 
     
-}
+// }
 
 // Scroll reveal
 
